11bddd997890ff22301feef5169d447bee66c41c
iqbaljlldn <iqbaljalaludin1309@gmail.com>
2025-10-02 00:38:42 +0700

    feat(session): add session repository methods
    - Added findByUserId method
    - Added deleteExpiredSessions method

--- src/domain/session/ISessionRepository.ts
+++ src/domain/session/ISessionRepository.ts
@@ -2,7 +2,9 @@ import { IGenericRepository } from "../common/IGenericRepository";
 import { Session } from "./Session";
 
 export interface ISessionRepository extends IGenericRepository<Session> {
+    findByUserId(userId: string): Promise<Session[]>
     findByJti(jti: string): Promise<Session | null>
     deactivateAllSessionByUserId(userId: string): Promise<boolean>
     deleteByJti(jti: string): Promise<boolean>
+    deleteExpiredSessions(): Promise<number>
 }
